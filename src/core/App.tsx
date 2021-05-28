4fbd0dbec666d1a8e178f34b8e9ebc76c0f0c297
carlos-yang <carlos.yang@kakaocorp.com>
2021-05-28 13:40:51 +0900

    [feat] sample button 추가

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@components/button/Button';
 import React from 'react';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">Learn React</header>
+      <Button onClick={() => console.log('click button')}>testButton</Button>
     </div>
   );
 }
